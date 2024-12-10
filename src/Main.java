a16bb41857ec1160298b67e4f2beca084256b133
nataliafrancisca <nathifrancisca@gmail.com>
2024-12-09 21:05:29 -0300

    feat: create code to request data from api

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final String API_KEY = "";
+        final String REQUEST_URL = "https://api.themoviedb.org/3/movie/popular";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(REQUEST_URL))
+                .header("accept", "application/json")
+                .header("Authorization", "Bearer ".concat(API_KEY))
+                .GET()
+//                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status: " + response.statusCode());
+        System.out.println("Body:" + response.body());
     }
 }
