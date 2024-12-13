42eedb607d7bf6d98dab894e4f7b46f2887077b4
nataliafrancisca <nathifrancisca@gmail.com>
2024-12-13 20:51:59 -0300

    feat: remove api key

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        final String API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YmFjNzg5NmFiY2RjZDJmYTU1MTZlNmQ2Yjg5YmFhMiIsIm5iZiI6MTYyMTY0NjQ5OC40NzMsInN1YiI6IjYwYTg1Y2EyMDM5OGFiMDA3N2M3MDllMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mdR4tlmhMLFEvpBHS4CV7k6dlFz7xOXm99M7YE3LmPM";
+        final String API_KEY = "";
         final String REQUEST_URL = "https://api.themoviedb.org/3/movie/popular";
 
         String response = new MoviesApiClient(API_KEY, REQUEST_URL).get();
