75058e333aa0209f49e62f5c08fd711943898999
nataliafrancisca <nathifrancisca@gmail.com>
2024-12-15 22:56:01 -0300

    feat: create class Content

---
+++ src/main/java/content/Content.java
@@ -0,0 +1,40 @@
+package content;
+
+public class Content {
+    private String title;
+    private String releaseDate;
+    private Double rating;
+    private String posterImage;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getPosterImage() {
+        return posterImage;
+    }
+
+    public void setPosterImage(String posterImage) {
+        this.posterImage = posterImage;
+    }
+}
