44f2b9f3fd26760cfd8a32fe3572d71c42a99383
nataliafrancisca <nathifrancisca@gmail.com>
2024-12-15 22:54:01 -0300

    feat: change class Movie to class Content

--- src/main/java/marvelapi/json/MarvelJsonParser.java
+++ src/main/java/marvelapi/json/MarvelJsonParser.java
@@ -1,17 +1,17 @@
 package marvelapi.json;
 
+import content.Content;
 import marvelapi.mapper.MarvelResponseMapper;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import interfaces.Parser;
-import movie.Movie;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MarvelJsonParser implements Parser<Movie> {
+public class MarvelJsonParser implements Parser<Content> {
     private final Gson gson;
 
     public MarvelJsonParser(){
@@ -19,15 +19,15 @@ public class MarvelJsonParser implements Parser<Movie> {
     }
 
     @Override
-    public List<Movie> parse(String movies){
-        JsonObject moviesAsJson = gson.fromJson(movies, JsonObject.class);
+    public List<Content> parse(String content){
+        JsonObject moviesAsJson = gson.fromJson(content, JsonObject.class);
         JsonArray moviesAsArray = moviesAsJson.get("data").getAsJsonObject().getAsJsonArray("results");
 
-        List<Movie> movieList = new ArrayList<>();
+        List<Content> movieList = new ArrayList<>();
 
         for(JsonElement element : moviesAsArray){
             JsonObject movieAsJson = element.getAsJsonObject();
-            Movie movie = new MarvelResponseMapper().mapResponse(movieAsJson);
+            Content movie = new MarvelResponseMapper().mapResponse(movieAsJson);
             movieList.add(movie);
         }
 
